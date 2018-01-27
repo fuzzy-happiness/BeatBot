64b2ae39b92b6e90d5abdf2b31c00600cb5abfff
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-01-27 15:40:53 +0800

    Added julius

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default class App extends React.Component {
   render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    }
     return (
       <View style={styles.container}>
+        <Text>Julius</Text>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        <Image source={pic} style={{width: 200, height: 200}}/>
       </View>
     );
   }
